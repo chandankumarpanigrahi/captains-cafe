f679bb447d80aa84e42863f5f460e45a7160a8f6
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-08-21 14:37:27 +0530

    Update index.js

--- src/components/design/testimonials/index.js
+++ src/components/design/testimonials/index.js
@@ -11,7 +11,7 @@ const Testimonials = () => {
     return (
         <div className='container animate__animated animate__fadeInUp mb-32'>
             <div className="flex flex-col lg:flex-row justify-center items-center">
-                <h1 className='color-primary text-4xl font-bold mb-10 text-center w-2/5'>What Our Happy and Fulfilled Customer Say's About Us</h1>
+                <h1 className='color-primary text-4xl font-bold mb-10 text-center w-2/5'>What Our Happy and Fulfilled Customer Say&apos;s About Us</h1>
                 <div className={`${styles.testimonials} flex flex-col pl-0 pr-0 md:pl-16 md:pr-12`}>
 
                 </div>
