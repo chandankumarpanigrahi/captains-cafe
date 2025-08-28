b017c1eb20da3b955e1be60aa1d1418adea43234
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-08-28 15:29:31 +0530

    Updated Loader

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import styles from "./loading.module.css"
+export default function Loading() {
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <div className={styles.loader}></div>
+        </div>
+    );
+}
+
