c0b8ffc3c0ea91dff1d02ba00451920bb41f47e8
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-09-01 11:49:05 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+});
+
+module.exports = withPWA({
+    reactStrictMode: true,
+});
+
 export default nextConfig;
