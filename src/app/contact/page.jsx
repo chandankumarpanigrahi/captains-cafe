6a800501ffa94625c888559a4690e41a7f39c7ee
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-09-23 17:50:51 +0530

    Updated the Contact us

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,57 +6,133 @@ import EmblaCarouselMultiple from '@/components/design/fade image';
 
 const ContactUs = () => {
     return (
-        <div className='container py-10'>
+        <>
+            <div className='container py-10'>
 
-            {/* Heading and Description */}
-            <div className="flex flex-col px-0 md:px-20 lg:px-80 w-full mb-16">
-                <h1 className='color-primary text-3xl md:text-4xl font-bold mb-3 text-center'>Get in Touch</h1>
-                <p className='text-[#374F67] text-md md:text-lg text-center'>We're here to help and answer any question you might have. We look forward to hearing from you.</p>
+                {/* Heading and Description */}
+                <div className="flex flex-col px-0 md:px-20 lg:px-80 w-full mb-16">
+                    <h1 className='color-primary text-3xl md:text-4xl font-bold mb-3 text-center'>Get in Touch</h1>
+                    <p className='text-[#374F67] text-md md:text-lg text-center'>We're here to help and answer any question you might have. We look forward to hearing from you.</p>
+                </div>
+
+                <div className='flex flex-col-reverse lg:flex-row items-center'>
+
+                    <div className='w-full lg:w-3/5 p-0 lg:px-12'>
+                        <div className="w-full">
+                            <EmblaCarouselMultiple />
+                        </div>
+                    </div>
+
+                    <div className='w-full lg:w-2/5 mb-12 lg:mb-0'>
+                        <section className='mb-12'>
+                            <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Visit Us</h3>
+                            <ol className='list-decimal pl-5 flex flex-col gap-2 text-gray-700 text-lg'>
+                                <li><a href="#" target='_blank'>Ekamra Kanan Road, near Chilika Fresh, Rental Colony, IRC Village, Nayapalli, Bhubaneswar, Odisha 751011</a></li>
+                                <li><a href="#" target='_blank'>Beside Government Veterinary Hospital, Maharishi College Rd, Saheed Nagar, Bhubaneswar, Odisha 751007</a></li>
+                            </ol>
+                        </section>
+                        <section className='mb-12'>
+                            <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Call Us</h3>
+                            <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
+                                <li><a href="tel:+918144774349" target='_blank'>+91 81447 74349</a></li>
+                            </ol>
+                        </section>
+                        <section className='mb-12'>
+                            <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Mail Us</h3>
+                            <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
+                                <li><a href="mailto:contact@thecaptainscafe.co.uk" target='_blank'>contact@thecaptainscafe.co.uk</a></li>
+                            </ol>
+                        </section>
+                        <section className='mb-10'>
+                            <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Café Hours</h3>
+                            <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
+                                <li>Monday to Sunday: 8:30 am - 10 pm</li>
+                            </ol>
+                        </section>
+                        <section className='flex flex-row gap-2'>
+                            <a href='#' target='_blank' className={`${styles.fb_icon}  p-1 text-white cursor-pointer rounded-full`}><LiaFacebook size={60} /></a>
+                            <a href='https://www.instagram.com/thecaptainscafe.india' target='_blank' className={`${styles.insta_icon}  p-1 text-white cursor-pointer rounded-full`}><LiaInstagram size={60} className={styles.icon} /></a>
+                        </section>
+                    </div>
+
+                </div>
             </div>
+            <div className="w-full flex bg-[#220F02] py-12">
+                <div className='container py-10 mx-auto relative'>
+                    <div className="flex flex-col lg:flex-row">
+                        <div className="w-full lg:w-1/2 flex flex-col gap-10 text-white">
+                            <h1 className='text-5xl p-4 leading-normal text-center lg:text-left'>Have any Query! <br />Let&apos;s discuss </h1>
+                            <p className='p-4'>Thank you for getting in touch! <br /> Kindly Fill the form, have a great day!</p>
+                        </div>
+                        <div className="w-full lg:w-1/2 flex flex-wrap gap-y-6 text-white">
+                            <div className="w-1/2 pr-3">
+                                <input
+                                    type="text"
+                                    placeholder="Your Name"
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-3">
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                />
+                            </div>
 
-            <div className='flex flex-col-reverse lg:flex-row items-center'>
+                            <div className="w-1/2 pr-3">
+                                <input
+                                    type="number"
+                                    placeholder="Phone Number"
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-3">
+                                <select
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                >
+                                    <option value="">Country</option>
+                                    <option value="India">India</option>
+                                    <option value="USA">USA</option>
+                                </select>
+                            </div>
 
-                <div className='w-full lg:w-3/5 p-0 lg:px-12'>
-                    <div className="w-full">
-                        <EmblaCarouselMultiple />
+                            <div className="w-1/2 pr-3">
+                                <input
+                                    type="text"
+                                    placeholder="Company Name"
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                />
+                            </div>
+                            <div className="w-1/2 pl-3">
+                                <select
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                >
+                                    <option value="">Interested in</option>
+                                    <option value="service1">Service 1</option>
+                                    <option value="service2">Service 2</option>
+                                </select>
+                            </div>
+
+                            <div className="w-full">
+                                <textarea
+                                    placeholder="Message"
+                                    rows="3"
+                                    className="w-full bg-transparent border-b border-amber-50 py-2 focus:outline-none"
+                                ></textarea>
+                            </div>
+
+                            <div className="w-full mt-4">
+                                <button className="bg-white text-black px-6 py-2 rounded">
+                                    SUBMIT
+                                </button>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-
-                <div className='w-full lg:w-2/5 mb-12 lg:mb-0'>
-                    <section className='mb-12'>
-                        <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Visit Us</h3>
-                        <ol className='list-decimal pl-5 flex flex-col gap-2 text-gray-700 text-lg'>
-                            <li><a href="#" target='_blank'>Ekamra Kanan Road, near Chilika Fresh, Rental Colony, IRC Village, Nayapalli, Bhubaneswar, Odisha 751011</a></li>
-                            <li><a href="#" target='_blank'>Beside Government Veterinary Hospital, Maharishi College Rd, Saheed Nagar, Bhubaneswar, Odisha 751007</a></li>
-                        </ol>
-                    </section>
-                    <section className='mb-12'>
-                        <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Call Us</h3>
-                        <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
-                            <li><a href="tel:+918144774349" target='_blank'>+91 81447 74349</a></li>
-                        </ol>
-                    </section>
-                    <section className='mb-12'>
-                        <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Mail Us</h3>
-                        <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
-                            <li><a href="mailto:contact@thecaptainscafe.co.uk" target='_blank'>contact@thecaptainscafe.co.uk</a></li>
-                        </ol>
-                    </section>
-                    <section className='mb-10'>
-                        <h3 className='text-3xl text-blue-900 font-semibold mb-3'>Café Hours</h3>
-                        <ol className='flex flex-col gap-1 text-gray-700 text-lg'>
-                            <li>Monday to Sunday: 8:30 am - 10 pm</li>
-                        </ol>
-                    </section>
-                    <section className='flex flex-row gap-2'>
-                        <a href='#' target='_blank' className={`${styles.fb_icon}  p-1 text-white cursor-pointer rounded-full`}><LiaFacebook size={60} /></a>
-                        <a href='https://www.instagram.com/thecaptainscafe.india' target='_blank' className={`${styles.insta_icon}  p-1 text-white cursor-pointer rounded-full`}><LiaInstagram size={60} className={styles.icon} /></a>
-                    </section>
-                </div>
-
             </div>
-
-        </div>
+        </>
     )
 }
 
