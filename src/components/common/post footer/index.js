bbe40bc1aaf1763ff2788fc555d00038367cca4e
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-09-24 12:48:12 +0530

    Updated Post Footer

--- src/components/common/post footer/index.js
+++ src/components/common/post footer/index.js
@@ -3,9 +3,9 @@ import { FaHeart } from "react-icons/fa";
 const PostFooter = () => {
     return (
         <div className='bg-[#021528] text-[#E1B89D] w-full flex'>
-            <div className="container mx-auto flex text-sm py-1.5">
-                <p>&copy; Copyright 2024-25, All Rights Reserved by The Captain&apos;s Cafe</p>
-                <p className='ml-auto flex items-center'>Made with <FaHeart className='text-red-600 mx-1.5'/> by <a href="https://shoolin.co.uk" className='font-semibold hover:text-white ml-1'>Shoolin Innovations Limited, UK</a></p>
+            <div className="container mx-auto flex flex-col lg:flex-row gap-y-1 text-[12px] md:text-sm py-1.5">
+                <p className='text-center lg:text-left'>&copy; Copyright 2024-25, All Rights Reserved by The Captain&apos;s Cafe</p>
+                <p className='mx-auto lg:mr-0 text-center'>Made with <FaHeart className='text-red-600 mx-1.5 inline-block'/> by <a href="https://shoolin.co.uk" className='font-semibold hover:text-white ml-1'>Shoolin Innovations Limited, UK</a></p>
             </div>
         </div>
     )
