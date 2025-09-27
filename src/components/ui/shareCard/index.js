4cc8de20b288f1ef4417add8ec96520a033bd51a
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-09-27 15:55:29 +0530

    Update index.js

--- src/components/ui/shareCard/index.js
+++ src/components/ui/shareCard/index.js
@@ -91,7 +91,7 @@ const ShareCard = ({ title = "Check this out!", className = "" }) => {
                 >
                     <div className="w-10 h-10 bg-black rounded-full flex items-center justify-center mb-2 group-hover:scale-110 transition-transform">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" class="bi bi-twitter-x" viewBox="0 0 16 16" id="Twitter-X--Streamline-Bootstrap" height="16" width="16">
-                            <path d="M12.6 0.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867 -5.07 -4.425 5.07H0.316l5.733 -6.57L0 0.75h5.063l3.495 4.633L12.601 0.75Zm-0.86 13.028h1.36L4.323 2.145H2.865z" stroke-width="1"></path>
+                            <path d="M12.6 0.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867 -5.07 -4.425 5.07H0.316l5.733 -6.57L0 0.75h5.063l3.495 4.633L12.601 0.75Zm-0.86 13.028h1.36L4.323 2.145H2.865z"></path>
                         </svg>
                     </div>
                     <span className="text-xs text-gray-600">X</span>
