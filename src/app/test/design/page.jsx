bd583f5f18db44cd393f00a52f394e00d1cb7d5c
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-10-16 16:55:20 +0530

    Added a Simple Table in Test > Design

--- src/app/test/design/page.jsx
+++ src/app/test/design/page.jsx
@@ -1,5 +1,15 @@
+"use client";
 import React from 'react'
 import SubBanner from '@/components/common/sub banner'
+
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
 
 const Design = () => {
     return (
@@ -8,10 +18,53 @@ const Design = () => {
                 title="Test Design"
                 breadcrumbItems={[
                     { label: "Test", href: "/test" },
-                    { label: "Design" } // No href = current page
+                    { label: "Design" }
                 ]}
             />
             <div className='container py-10'>
+                <Table className="bg-white text-center border">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-30 text-center">Invoice</TableHead>
+                            <TableHead  className="text-center">Status</TableHead>
+                            <TableHead  className="text-center">Method</TableHead>
+                            <TableHead className="text-center">Amount</TableHead>
+                            <TableHead className="w-40 text-center">Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">DFHD5436523</TableCell>
+                            <TableCell>7587654</TableCell>
+                            <TableCell>Online</TableCell>
+                            <TableCell className="text-right">85478</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">DFHD5436523</TableCell>
+                            <TableCell>7587654</TableCell>
+                            <TableCell>Online</TableCell>
+                            <TableCell className="text-right">85478</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">DFHD5436523</TableCell>
+                            <TableCell>7587654</TableCell>
+                            <TableCell>Online</TableCell>
+                            <TableCell className="text-right">85478</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">DFHD5436523</TableCell>
+                            <TableCell>7587654</TableCell>
+                            <TableCell>Online</TableCell>
+                            <TableCell className="text-right">85478</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">DFHD5436523</TableCell>
+                            <TableCell>7587654</TableCell>
+                            <TableCell>Online</TableCell>
+                            <TableCell className="text-right">85478</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         </>
     )
