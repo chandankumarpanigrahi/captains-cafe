b26fd91b776f749fd4ca4a1567eb95efcd7b7ca9
Chandan Kumar Panigrahi <ckpanigrahi1996@gmail.com>
2025-10-29 11:22:17 +0530

    Theme updates

--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -43,7 +43,7 @@ const Profile = () => {
       </div>
       <div className="w-full px-6 py-4 rounded-md bg-white dark:bg-neutral-800 shadow-[inset_-4px_-4px_4px_-1px_rgba(0,0,0,0.15)] border border-gray-100">
         <div className="flex justify-between items-center mb-3">
-          <h2 className="text-2xl font-semibold text-blue-900">Personal Information</h2>
+          <h2 className="text-2xl font-semibold text-blue-900 dark:text-white">Personal Information</h2>
           <Sheet>
             <SheetTrigger>
               <Button text="EDIT" radius="md" size='sm' />
@@ -54,23 +54,23 @@ const Profile = () => {
                 <SheetDescription className=" py-2 px-4">
                   <form>
                     <div className="mb-3">
-                      <Label className="text-gray-800">Your Name</Label>
+                      <Label className="text-gray-800 dark:text-gray-300">Your Name</Label>
                       <Input type="text" placeholder="Enter Your Name" value="Subham Choudhury" />
                     </div>
                     <div className="mb-3">
-                      <Label className="text-gray-800">Mobile Number</Label>
+                      <Label className="text-gray-800 dark:text-gray-300">Mobile Number</Label>
                       <Input type="text" placeholder="Enter Your Mobile Number" value="+91 89599 78588" />
                     </div>
                     <div className="mb-3">
-                      <Label className="text-gray-800">Alternate Mobile Number</Label>
+                      <Label className="text-gray-800 dark:text-gray-300">Alternate Mobile Number</Label>
                       <Input type="text" placeholder="Enter Alternate Mobile Number" value="+91 78554 98665" />
                     </div>
                     <div className="mb-3">
-                      <Label className="text-gray-800">Email Address</Label>
+                      <Label className="text-gray-800 dark:text-gray-300">Email Address</Label>
                       <Input type="email" placeholder="Enter Your Email Address" value="subham787898@gmail.com" />
                     </div>
                     <div className="mb-3">
-                      <Label className="text-gray-800">Upload Your Profile Picture</Label>
+                      <Label className="text-gray-800 dark:text-gray-300">Upload Your Profile Picture</Label>
                       <Input />
                     </div>
                     <div className="mb-3 flex flex-row">
@@ -113,7 +113,7 @@ const Profile = () => {
 
       <div className="w-full px-6 py-4 rounded-md bg-white dark:bg-neutral-800 shadow-[inset_-4px_-4px_4px_-1px_rgba(0,0,0,0.15)] border border-gray-100">
         <div className="flex justify-between items-center mb-3">
-          <h2 className="text-2xl font-semibold text-blue-900">User Information</h2>
+          <h2 className="text-2xl font-semibold text-blue-900 dark:text-white">User Information</h2>
         </div>
         <hr className='my-2 border-gray-200' />
         <div className="flex flex-wrap justify-between gap-3 text-gray-600 text-[15px] mt-4">
